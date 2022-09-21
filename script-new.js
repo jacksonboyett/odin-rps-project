15d2afd531d21440c1d7c6db76d73572c0a81ca4
jacksonboyett <jackson.boyett@gmail.com>
2022-09-21 17:36:50 +0200

    added buttons to play game

--- script-new.js
+++ script-new.js
@@ -1,5 +1,5 @@
 // Generate a computer choice for Rock, Paper, or Scissors
-function singleGame() {
+// function singleGame() {
 function getComputerChoice(randomNumber) {
     randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -14,31 +14,31 @@ function getComputerChoice(randomNumber) {
 getComputerChoice();
 console.log(`Computer choice: ${computerChoice}`)
 
-// Ask the user for their choice
-function askUser() {
-    playerOutput = prompt('Play three games of rock, paper, scissors with the computer! Please enter your selection below:')
-    return playerOutput
-}
-let playerChoice = askUser();
+// // Ask the user for their choice
+// function askUser() {
+//     playerOutput = prompt('Play three games of rock, paper, scissors with the computer! Please enter your selection below:')
+//     return playerOutput
+// }
+// let playerChoice = askUser();
 
-// Standardize the user selection
-function capitalizeChoice() {
-    let firstLetter = playerChoice.slice(0,1);
-    firstLetter = firstLetter.toUpperCase();
-    let restString = playerChoice.slice(1);
-    restString = restString.toLowerCase();
-    correctedChoice = firstLetter + restString;
-    return correctedChoice
-}
-capitalizeChoice(playerChoice);
+// // Standardize the user selection
+// function capitalizeChoice() {
+//     let firstLetter = playerChoice.slice(0,1);
+//     firstLetter = firstLetter.toUpperCase();
+//     let restString = playerChoice.slice(1);
+//     restString = restString.toLowerCase();
+//     correctedChoice = firstLetter + restString;
+//     return correctedChoice
+// }
+// capitalizeChoice(playerChoice);
 
-// Check to see if the user entered a valid response
-while (correctedChoice != "Rock" && correctedChoice != 'Scissors' && correctedChoice != 'Paper') {
-    alert('You must input either Rock, Paper, or Scissors');
-    playerChoice = askUser();
-    correctedChoice = capitalizeChoice(playerChoice);
-}
-console.log(`Player choice: ${correctedChoice}`);
+// // Check to see if the user entered a valid response
+// while (correctedChoice != "Rock" && correctedChoice != 'Scissors' && correctedChoice != 'Paper') {
+//     alert('You must input either Rock, Paper, or Scissors');
+//     playerChoice = askUser();
+//     correctedChoice = capitalizeChoice(playerChoice);
+// }
+// console.log(`Player choice: ${correctedChoice}`);
 
 // Compare Choices
 function compareChoice() {
@@ -69,32 +69,33 @@ function compareChoice() {
     }
     return result;
 };
-compareChoice(correctedChoice, computerChoice);
-};
-// singleGame();
-// console.log(result);
 
-// Play a 5 round game
-// Create the game function
-// Call the single game function five times
-// Tally results
-// Output results
+// };
 
-function game() {
-    let tally = 0;
-    for (let i = 0; i < 3; i++) {
-        singleGame();
-        tally = tally + result.counter;
-    }
-    total = tally;
-    return total
-};
-game();
+const container = document.querySelector('.container');
 
-if (total > 0) {
-    alert('!!! YOU WIN !!!');
-} else if (total = 0) {
-    alert('You tied with the computer');
-} else {
-    alert(':( YOU LOST :(');
-};
+const rockBtn = document.querySelector('.rock');
+rockBtn.addEventListener('click', function (e) {
+    correctedChoice = 'Rock';
+    compareChoice(correctedChoice, computerChoice);
+    const resultDiv = document.createElement('div')
+    resultDiv.textContent = result.message;
+    container.appendChild(resultDiv)
+})
+const paperBtn = document.querySelector('.paper');
+paperBtn.addEventListener('click', function (e) {
+    correctedChoice = 'Paper';
+    compareChoice(correctedChoice, computerChoice);
+    const resultDiv = document.createElement('div')
+    resultDiv.textContent = result.message
+    container.appendChild(resultDiv)
+})
+const scissorsBtn = document.querySelector('.scissors');
+scissorsBtn.addEventListener('click', function (e) {
+    correctedChoice = 'Scissors';
+    compareChoice(correctedChoice, computerChoice);
+    const resultDiv = document.createElement('div')
+    resultDiv.textContent = result.message;
+    container.appendChild(resultDiv)
+})
+
